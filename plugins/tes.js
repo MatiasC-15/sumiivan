65e374003d22166f68a2dcf6eabac5dde2a4b8ea
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-02-11 21:59:17 -0300

    Actualizar tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -66,9 +66,9 @@ let handler = async (m, { conn, args }) => {
   await m.react('✅');
 };
 
-handler.help = ['play *<texto>*'];
+handler.help = ['w *<texto>*'];
 handler.tags = ['dl'];
-handler.command = ['play'];
+handler.command = ['w'];
 
 export default handler;
 
